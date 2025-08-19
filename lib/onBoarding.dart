a2d8ac3a24a7d67424222d7d3934f1b78389cfc4
Suman <gundapanthulasuman@gmail.com>
2025-08-19 23:13:31 +0530

    updated onboarding screen

--- lib/onBoarding.dart
+++ lib/onBoarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -8,41 +10,73 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  final PageController controller = PageController();
+  int currentPageIndex=0;
+  var onBoardingItemsList = [
+    {"icon":"https://cdn-icons-gif.flaticon.com/18124/18124722.gif", "title":"Exclusive Discounts", "description": "Get the best deals on fresh groceries and daily discounts"},
+    {"icon":"https://cdn-icons-gif.flaticon.com/11933/11933522.gif", "title":"Fast \n Delivery", "description": "Get the best deals on fresh groceries and daily discounts"},
+    {"icon":"https://cdn-icons-gif.flaticon.com/18873/18873948.gif", "title":"Seamless Payments", "description": "Get the best deals on fresh groceries and daily discounts"}
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
     body: SafeArea(
       child: Stack(
         children: [
           PageView.builder(
-            itemCount: 3,
+            controller: controller,
+            physics: BouncingScrollPhysics(),
+            itemCount: onBoardingItemsList.length,
+              onPageChanged: (index){
+              setState(() {
+                currentPageIndex = index;
+              });
+              },
               itemBuilder: (context, index){
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.network("https://cdn-icons-gif.flaticon.com/15576/15576195.gif",
-                    height: 200),
-                    Text("Exclusive Discounts",
-                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, height: 1.25),textAlign: TextAlign.center,),
-                    SizedBox(height: 20.0),
-                    Text("Get the best deals on fresh groceries and daily discounts",
-                      style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
-                    SizedBox(height: 40.0),
-                    ElevatedButton(onPressed: ()=>{},
-                        child: Text("Next", style: TextStyle(color: Colors.white),),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(300,50),
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)
-                        )
+                return Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network("${onBoardingItemsList[index]["icon"]}",
+                      height: 200),
+                      Text("${onBoardingItemsList[index]["title"]}",
+                      style: GoogleFonts.lato(fontSize: 50, fontWeight: FontWeight.bold, height: 1.25, color: Color(0xFF010131)),textAlign: TextAlign.center,),
+                      SizedBox(height: 20.0),
+                      Text("${onBoardingItemsList[index]["description"]}",
+                        style: GoogleFonts.lato(fontSize: 20), textAlign: TextAlign.center),
+                      SizedBox(height: 40.0),
+                      ElevatedButton(onPressed: ()=>{
+                        if(currentPageIndex < onBoardingItemsList.length -1){
+                          controller.nextPage(duration: Duration(microseconds: 500), curve: Curves.easeInOut)
+                        }
+                      },
+                          child: Text("Next", style: GoogleFonts.lato(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(300,50),
+                          backgroundColor: Color(0xFF010131),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)
+                          )
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20.0)
-                  ],
+                      SizedBox(height: 30.0),
+                      AnimatedSmoothIndicator(
+                        count: onBoardingItemsList.length,
+                        effect: SlideEffect(
+                            dotHeight: 12,
+                            dotWidth: 12,
+                            dotColor: Colors.grey,
+                          activeDotColor: Color(0xFF010131)
+                        ), activeIndex: index,
+                      )
+                    ],
+                  ),
                 );
               }
-          )
+          ),
+
         ],
       ),
     )
