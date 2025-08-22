e70d235e641d3ee1a6a7a75b8ddde19ce1cf0aba
suman98490 <62735189+suman98490@users.noreply.github.com>
2025-08-22 15:35:03 +0530

    Committing changes
    Committing changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3370720300"
+    serviceWorkerVersion: "3845114563"
   }
 });
